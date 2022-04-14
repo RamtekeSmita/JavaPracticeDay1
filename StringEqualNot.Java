a64d2949e50c0dcf1247eab704f4201bb0632af4
Smita <smita140591@gmail.com>
2022-04-14 16:53:41 +0530

    Added: StringEqualNot

---
+++ StringEqualNot.Java
@@ -0,0 +1,12 @@
+package com.Bridgelabz;
+
+public class StringEqualNot {
+    public static void main(String[] args) {
+        String str1 = "Smita";
+        String str2 = "Smita";
+        String str3 = "maits";
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+    }
+}
